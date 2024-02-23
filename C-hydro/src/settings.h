f3ffb3b556325319e7a3994d0b6774a14dbabb34
Hendrik van Eerten <hveerten@gmail.com>
2024-02-23 12:43:16 +0000

    cleaning up presentation

--- C-hydro/src/settings.h
+++ C-hydro/src/settings.h
@@ -1,7 +1,6 @@
-// settings.h(.template)
+// settings.h
 //
-// If the template file, copy this to settings.h and modify settings.h to
-// the specific scenario to be run. Settings.h will remain untracked in git
+// change this file for problem-specific settings
 
 #ifndef SETTINGS_H_
 #define SETTINGS_H_
@@ -11,9 +10,9 @@
 #define parallelization_method_ 0
 
 #define DIMS_   1 // number of dimensions
-#define RES_x_ 200 // x-direction resolution, not counting ghost zones
-#define RES_y_ 50 // y-direction resolution, not counting ghost zones
-#define RES_z_ 50 // z-direction resolution, not counting ghost zones
+#define RES_x_  200 // x-direction resolution, not counting ghost zones
+#define RES_y_  50 // y-direction resolution, not counting ghost zones
+#define RES_z_  50 // z-direction resolution, not counting ghost zones
 #define ghosts_ 2  // number of ghost cells on a side
 #define it_max_ 1000000 // maximum number of iterations
 
@@ -27,32 +26,31 @@
   // 02  a Rayleigh-Taylor problem (requires at least 2 dimensions)
   // 03  a mock central gravity problem
 
-#define no_snapshots_ 101 // how many data dumps to write to disc
+#define no_snapshots_ 2 // how many data dumps to write to disc
 #define verbose_ 1 // set to zero (FALSE) for less output to terminal
 
 // boundary conditions
 // 0 is outflow, 1 is reflecting, 2 is periodic, 3 is user (3 not implemented)
-#define x_min_boundary_ 2
-#define x_max_boundary_ 2
+#define x_min_boundary_ 0
+#define x_max_boundary_ 0
 #define y_min_boundary_ 1
 #define y_max_boundary_ 1
 #define z_min_boundary_ 2
 #define z_max_boundary_ 2
 
-#define dt_floor 1e-9
-#define p_floor_ 1e-5
+#define dt_floor   1e-9
+#define p_floor_   1e-5
 #define rho_floor_ 1e-5
-#define E_floor_ 1e-10
+#define E_floor_   1e-10
 
 #define CFL_factor_ 0.3 // scale factor when setting CFL timestep 
 
 // Start with fixed index polytropic gas
-// with gamma = 5/3
-#define GAMMA_AD_ 1.4 // 1.6666666666666
+#define GAMMA_AD_ 1.4
 
 // flags to revert to lowest order methods
-#define disable_RK_ true // drop from 3rd order Runge-Kutta to first order ------------- fix this, code does not work if disabled?
-  // forward Euler timestep if true
+#define disable_RK_ true // drop from 3rd order Runge-Kutta to first order 
+ // forward Euler timestep if true
 #define disable_PLM_ true // drop to piecewise constant if true
 #define use_source_ 0 // 1 // non-zero number to use source
 
